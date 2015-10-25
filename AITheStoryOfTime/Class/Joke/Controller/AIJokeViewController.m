a6e6ca1c98e4ec0aac705101be4ff69ef2e442f2
aizexin <443792431@qq.com>
2015-10-25 22:15:36 +0800

    实现笑话的刷新加载，但是数据没有顺序

--- AITheStoryOfTime/Class/Joke/Controller/AIJokeViewController.m
+++ AITheStoryOfTime/Class/Joke/Controller/AIJokeViewController.m
@@ -16,9 +16,17 @@
 #import "AIJokeGroupModel.h"
 #import "AIJokeCellFrameModel.h"
 #import "AIJokeDefine.h"
+#import "NSDate+DateTools.h"
+#import "MJRefresh.h"
 @interface AIJokeViewController ()<UITableViewDelegate,UITableViewDataSource>
 @property(nonatomic,weak)UITableView *tableView;
 @property(nonatomic,strong)NSMutableArray *dataSourceM;
+@property(nonatomic,weak)UIRefreshControl *refresh;
+@property(nonatomic,assign,getter=isLoading)BOOL loading;
+/**
+ *  第一次进来的时间
+ */
+@property(nonatomic,strong)NSDate *fristDate;
 @end
 
 @implementation AIJokeViewController
@@ -34,10 +42,12 @@
 #pragma mark --------------生命周期--------------
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.fristDate = [NSDate date];
     self.title = @"笑话";
     self.automaticallyAdjustsScrollViewInsets = NO;
     [self initTableView];
-    [self loadData];
+    //加载刷新控件
+    [self setupRefresh];
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(shareBtnDidSelected:) name:AIJokeShareEventNotification object:nil];
 }
 
@@ -48,13 +58,9 @@
                                                                                                                                                      UMShareToSina,UMShareToWechatSession,UMShareToWechatTimeline,UMShareToWechatFavorite,UMShareToQQ,
                                                                                                                                                      UMShareToQzone,
                                                                                                                                                      nil] delegate:self];
-    //    //分享微信的时候选择消息类型
-    //    //1.纯图片
-    //    [UMSocialData defaultData].extConfig.wxMessageType = UMSocialWXMessageTypeImage;
-    //    //2.纯文字，点击不会跳转
+    //2.纯文字，点击不会跳转
         [UMSocialData defaultData].extConfig.wxMessageType = UMSocialWXMessageTypeText;
-    //    //3.分享本应用，应用地址是微信开放平台填写的地址
-    //    [UMSocialData defaultData].extConfig.wxMessageType = UMSocialWXMessageTypeApp;
+  
 }
 
 #pragma mark ---------------------初始化方法-----------------
@@ -93,24 +99,98 @@
 }
 
 #pragma mark ----------------请求数据----------
--(void)loadData{
+
+/**
+ *  集成刷新控件
+ */
+- (void)setupRefresh{
+    UIRefreshControl *refresh = [[UIRefreshControl alloc]init];
+    self.refresh = refresh;
+    [self.tableView addSubview:refresh];
+    //2.监听refresh
+    [refresh addTarget:self action:@selector(refreshControlStateChange:) forControlEvents:(UIControlEventValueChanged)];
+    //3.让刷新空间自动进入刷新状态
+    [refresh beginRefreshing];
+    //4.加载数据
+    [self refreshControlStateChange:refresh];
+    //5.添加上拉加载更多控件
+    //上啦加载
+    [self refreshAndLoad];
+}
+/**
+ *  刷新方法
+ */
+-(void)refreshControlStateChange:(UIRefreshControl*)refreshControl{
+    
     AIJokeParamModel *params = [[AIJokeParamModel alloc]init];
+    NSInteger second = (NSInteger)[[NSDate date] secondsFrom:_fristDate];
+    AILog(@"--------%ld",second);
+    params.min_time = [NSString stringWithFormat:@"%ld",1445776000+second];
     [AIJokeTool JokeWithParams:params success:^(AIJokeCellModel *resultModel) {
-        AILog(@"%@",resultModel.data.data);
-        
+//        AILog(@"%@",resultModel.data.data);
+        if (self.dataSourceM) {
+            [self.dataSourceM removeAllObjects];
+        }
         NSMutableArray *arrayM = [NSMutableArray arrayWithArray:resultModel.data.data];
         for (AIJokeContentDataModel *contentData in arrayM) {
             AIJokeCellFrameModel *frameModel = [[AIJokeCellFrameModel alloc]init];
             frameModel.groupModel = contentData.group;
-            [self.dataSourceM addObject:frameModel];
+            [self.dataSourceM insertObject:frameModel atIndex:0];
+            // 让刷新控件停止刷新（恢复默认的状态）
+            [refreshControl endRefreshing];
+        }
+        //刷新表格
+        [self.tableView reloadData];
+    } failure:^(NSError *error) {
+        AILog(@"请求笑话数据失败%@",error.description);
+        // 让刷新控件停止刷新（恢复默认的状态）
+        [refreshControl endRefreshing];
+    }];
+    
+}
+
+-(void)refreshAndLoad{
+ 
+    //2.加载
+    [self.tableView addFooterWithCallback:^{
+        if (self.isLoading) {
+            return ;
+        }
+        self.loading = YES;
+        //重新加载数据
+        _loading = NO;
+        [self loadMoreData];
+        [self.tableView footerEndRefreshing];
+    }];
+    
+}
+/**
+ *  加载更多数据
+ */
+-(void)loadMoreData{
+    AIJokeParamModel *params = [[AIJokeParamModel alloc]init];
+    NSInteger second = (NSInteger)[[NSDate date] secondsFrom:_fristDate];
+    AILog(@"--------%ld",second);
+    params.min_time = [NSString stringWithFormat:@"%ld",1445776000-second];
+    [AIJokeTool JokeWithParams:params success:^(AIJokeCellModel *resultModel) {
+        //        AILog(@"%@",resultModel.data.data);
+        if (self.dataSourceM) {
+            [self.dataSourceM removeAllObjects];
+        }
+        NSMutableArray *arrayM = [NSMutableArray arrayWithArray:resultModel.data.data];
+        for (AIJokeContentDataModel *contentData in arrayM) {
+            AIJokeCellFrameModel *frameModel = [[AIJokeCellFrameModel alloc]init];
+            frameModel.groupModel = contentData.group;
+            [self.dataSourceM insertObject:frameModel atIndex:0];
         }
         //刷新表格
         [self.tableView reloadData];
     } failure:^(NSError *error) {
         AILog(@"请求笑话数据失败%@",error.description);
     }];
+    
+    [self.tableView reloadData];
 }
-
 
 
 
